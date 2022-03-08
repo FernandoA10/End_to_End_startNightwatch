358e191a98b9272dfa03780c7bfb008e4a1cb8db
L. Fernando Anda <l.fernandoanda@192.168.68.111>
2022-03-08 17:55:45 -0600

    prueba commit

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,7 +2,8 @@ module.exports = {
     // An array of folders (excluding subfolders) where your tests are located;
     // if this is not specified, the test source must be passed as the second argument to the test runner.
     src_folders: ["tests"],
-  
+    page_objects_path: ['page-objects'],
+      
     webdriver: {
       start_process: true,
       port: 9515,
@@ -19,4 +20,4 @@ module.exports = {
       }
     }
   };
-  
+   
